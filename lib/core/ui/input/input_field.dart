b50ca3572b0c23861a79da9a4398fcd91acf2a15
Wamitinewton <wamitinewton@gmail.com>
2024-08-04 20:28:34 +0300

    input fields complete

--- lib/core/ui/input/input_field.dart
+++ lib/core/ui/input/input_field.dart
@@ -1,3 +1,4 @@
+import 'package:athena_ai/core/extension/context.dart';
 import 'package:athena_ai/core/util/validators.dart';
 import 'package:flutter/material.dart';
 
@@ -12,20 +13,19 @@ class InputField extends StatefulWidget {
       this.validator,
       this.onFieldSubmitted});
 
-      const InputField.api({
-        required TextEditingController controller,
-        String label = "Gemini API Key",
-        TextInputAction textInputAction = TextInputAction.done,
-        Key? key,
-      }): this(
-        key: key,
-        controller: controller,
-        label: label,
-        textInputAction: textInputAction,
-        textInputType: TextInputType.url,
-        autofillHints: const [AutofillHints.url],
-        validator: Validators.apiKey
-      );
+  const InputField.api({
+    required TextEditingController controller,
+    String label = "Gemini API Key",
+    TextInputAction textInputAction = TextInputAction.done,
+    Key? key,
+  }) : this(
+            key: key,
+            controller: controller,
+            label: label,
+            textInputAction: textInputAction,
+            textInputType: TextInputType.url,
+            autofillHints: const [AutofillHints.url],
+            validator: Validators.apiKey);
 
   final TextEditingController controller;
   final String label;
@@ -40,8 +40,47 @@ class InputField extends StatefulWidget {
 }
 
 class _InputFieldState extends State<InputField> {
+  late bool obscureText;
+  late bool isPassword;
+
+  @override
+  void initState() {
+    isPassword = widget.textInputType == TextInputType.visiblePassword;
+    obscureText = isPassword;
+    super.initState();
+  }
+
+  @override
+  void didUpdateWidget(covariant InputField oldWidget) {
+    if (oldWidget.textInputType != widget.textInputType) {
+      isPassword = widget.textInputType == TextInputType.visiblePassword;
+      obscureText = isPassword;
+    }
+    super.didUpdateWidget(oldWidget);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final validator =
+        widget.validator ?? Validators.getValidator(widget.textInputType);
+    return TextFormField(
+      controller: widget.controller,
+      keyboardType: widget.textInputType,
+      obscureText: obscureText,
+      validator: validator,
+      autofillHints: widget.autofillHints,
+      decoration: InputDecoration(
+          labelText: widget.label,
+          suffixIcon: isPassword
+              ? IconButton(
+                  onPressed: () => setState(() => obscureText = !obscureText),
+                  icon: obscureText
+                      ? const Icon(Icons.visibility_off)
+                      : const Icon(Icons.visibility_sharp))
+              : null,
+          focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(8),
+              borderSide: BorderSide(color: context.colorScheme.onSurface))),
+    );
   }
 }
